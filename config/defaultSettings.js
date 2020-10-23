7301ea5905eadadaa7d9ce3600fb433b441a409b
baijz <baijz@winning.com.cn>
2020-10-23 11:24:35 +0800

    初始化仓库

---
+++ config/defaultSettings.js
@@ -0,0 +1,18 @@
+const proSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'side',
+
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '//at.alicdn.com/t/font_1338425_4tmamc43g4w.js',
+};
+export default proSettings;
